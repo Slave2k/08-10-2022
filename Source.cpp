7ffe9bc4f842684228a518b8d2958789f3a022e8
Slave2k <114007371+Slave2k@users.noreply.github.com>
2022-10-08 18:52:51 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	//1
+	/*int hp;
+	bool r;
+	cin >> hp;
+	if (hp > 0)
+		r = 1;
+	if (hp < 0)
+		r = 0;
+	cout << boolalpha << r;*/
+
+	//2
+	/*int on, off, a;
+	cin >> a;
+	if (a > 20)
+		cout << "on" << endl;
+	else if (a <= 20)
+		cout << "off" << endl;*/
+
+	//3
+	/*int v, t;
+	int Tp;
+	cin >> v >> Tp >> t;
+	if (v << 80 && Tp < t)
+	{
+		cout << "on" << endl;
+	}
+	else {
+		cout << "off" << endl;
+	}*/
+		//4
+	/*double x, y;
+	cin >> x;
+	if (x > 0) {
+		y = sqrt(x - 2);
+	}
+	else if (x == 0) {
+		y = x;
+	}
+	else if (x < 0) {
+		y = abs(x);
+	}
+	cout << y << endl;*/
+
+	//5
+	/*int A;
+	cin >> A;
+	switch(A) {
+	case 1: cout << "bolshoi palec" << endl; break;
+	case 2: cout << "ykazatekni palec" << endl; break;
+	case 3: cout << "sredni palec" << endl; break;
+	case 4: cout << "bezimani palec" << endl; break;
+	case 5: cout << "mizinec" << endl; break;
+	default: cout << "error" << endl;
+}*/
+
+}
